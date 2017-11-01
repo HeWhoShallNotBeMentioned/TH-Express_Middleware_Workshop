84123da004e0d6351ee5dbe512b6ffaa73b9342a
Chris Underwood <cunderwoodmn@gmail.com>
2017-11-01 13:46:26 -0700

    add new project files from treehouse. Add gitignore for node modules

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 const express = require('express');
-const fav = require('./fav');
-const app = express();
-app.use(fav);
-    //'/one',
-app.use( (req, res, next)=>{
-  //req.message = 'This message made it!';
-  console.log("hello");
-  const err = new Error('Holy Smokes!');
-  next(err);
-}
-// , (req, res, next)=>{
-//   console.log("one point five");
-//   next();
-// }
-);
+const path = require('path');
+const bodyParser = require('body-parser');
 
-app.use((req, res, next)=>{
-  console.log("World");
-  //console.log(req.message);
-  next();
+const routes = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.use((req, res) => res.send('<h1>Express is working!</h1>'));
-app.listen(3000);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
