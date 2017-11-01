ad265e116816f815b8c89234d88160e6c1ff78a2
Chris Underwood <cunderwoodmn@gmail.com>
2017-10-31 20:31:37 -0700

    seeing how we need to terminate middleware and how errors can be passed via next()

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const fav = require('./fav');
 const app = express();
 app.use(fav);
-
-app.use('/one', (req, res, next)=>{
-  req.message = 'This message made it!';
-  console.log("one");
-  next();
-}, (req, res, next)=>{
-  console.log("one point five");
-  next();
-});
+    //'/one',
+app.use( (req, res, next)=>{
+  //req.message = 'This message made it!';
+  console.log("hello");
+  const err = new Error('Holy Smokes!');
+  next(err);
+}
+// , (req, res, next)=>{
+//   console.log("one point five");
+//   next();
+// }
+);
 
 app.use((req, res, next)=>{
-  console.log(req.message);
+  console.log("World");
+  //console.log(req.message);
   next();
 });
 
